b69b94b21c540d680bf00e3afdced2798ae5610c
Ezequiel Falcon <ezequiel.falcon@globant.com>
2019-09-02 19:42:08 -0300

    Working filter

--- ejercicio6/js/ejercicio6.js
+++ ejercicio6/js/ejercicio6.js
@@ -1,44 +1,45 @@
-document.querySelector("#filter").addEventListener("click", () => {
-    toGrayScale(image);
-});
+
 
 var image = new Image();
+image.crossOrigin = "Anonymous";
 image.src = "image.jpg";
 
 image.onload = function() {
   drawThatImage(this)
+  document.querySelector("#filter").addEventListener("click", function() {
+    toGrayScale();
+  });
 }
 
-function drawThatImage(image){
+function drawThatImage(img){
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    ctx.drawImage(image, 0, 0);
-}
-
-async function toGrayScale(image) {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    console.log(canvas.width)
-    canvas.width = image.width;
-    canvas.height = image.height;
-    ctx.drawImage(image, 0, 0);
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
     var imageData=ctx.getImageData(0,0, canvas.width, canvas.height);
-    
+    console.log(imageData)
+}
+
+function toGrayScale() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    var imageData=ctx.getImageData(0,0, canvas.width, canvas.height);
+    console.log(imageData)
     for (i = 0; i < imageData.width; i++) {
       for(j = 0; j < imageData.height; j++) {
-        index = (i+j * imageData.width) * 4;
+        var index=(j*4)*imageData.width+(i*4);
          var r=imageData.data[index];
          var g=imageData.data[index+1];
          var b=imageData.data[index+2];
-         var a=imageData.data[index+3];
          var average=(r+g+b)/3;
-        setPixel(imageData, i, j, average,average,average,average)
+        setPixel(imageData, i, j, average,average,average,255)
        }
      }
+     ctx.putImageData(imageData, 0,0);
+     
+     console.log(imageData)
      console.log("Finished execution");
-
 }
 
 function setPixel(imageData, i, j, r, g, b, a){
